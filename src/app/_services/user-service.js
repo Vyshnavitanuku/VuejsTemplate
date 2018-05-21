c589cdde4825cbf50ea0453712af936ce6fb24ce
vyshnavi.tanuku <vyshnavi.tanuku@GGKU4SYS40>
2018-05-21 15:19:23 +0530

    misc changes

--- src/app/_services/user-service.js
+++ src/app/_services/user-service.js
@@ -3,8 +3,11 @@ import { vuePost } from '../utility/helper'
 
 export const loginService = async (loginDetails) => {
   return new Promise((resolve, reject) => {
-    vuePost(authEndpoint, loginDetails, { emulateJSON: true }).then(data => resolve(data)).catch(err => {
-      reject(err)
-    });
+    resolve(true);
+    //vuePost(authEndpoint, loginDetails, { emulateJSON: true }).then(data => 
+    //{debugger;
+    //resolve(data)}).catch(err => {
+    // reject(err)
+    //});
   });
 }
